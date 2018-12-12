82ac3b949a589c8162e33ea69ad20b691894ffff
sp <995199235@qq.com>
2018-12-12 16:07:21 +0800

    first commit

---
+++ LinearRegression.py
@@ -0,0 +1,60 @@
+import torch
+import torch.nn as nn
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Hyper-parameters
+input_size = 1
+output_size = 1
+num_epochs = 60
+learning_rate = 0.001
+
+# Toy dataset
+x_train = np.array(
+    [[3.3], [4.4], [5.5], [6.71], [6.93], [4.168], [9.779], [6.182], [7.59],
+     [2.167], [7.042], [10.791], [5.313], [7.997], [3.1]],
+    dtype=np.float32)
+
+y_train = np.array(
+    [[1.7], [2.76], [2.09], [3.19], [1.694], [1.573], [3.366], [2.596], [2.53],
+     [1.221], [2.827], [3.465], [1.65], [2.904], [1.3]],
+    dtype=np.float32)
+
+# Linear regression model
+model = nn.Linear(input_size, output_size)
+
+# Loss and optimizer
+criterion = nn.MSELoss()
+optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
+
+# Train the model
+for epoch in range(num_epochs):
+    # Convert numpy arrays to torch tensors
+    inputs = torch.from_numpy(x_train)
+    targets = torch.from_numpy(y_train)
+
+    # Forward pass
+    outputs = model(inputs)
+    loss = criterion(outputs, targets)
+
+    # Backward and optimize
+    # 根据pytorch中的backward()函数的计算，当网络参量进行反馈时，梯度是被积累的而不是被替换掉；
+    # 但是在每一个batch时毫无疑问并不需要将两个batch的梯度混合起来累积，因此这里就需要每个batch设置一遍zero_grad 了。
+    optimizer.zero_grad()
+    loss.backward()
+    # 这是大多数optimizer所支持的简化版本。一旦梯度被如backward()之类的函数计算好后，我们就可以调用这个函数。
+    optimizer.step()
+
+    if (epoch + 1) % 5 == 0:
+        print('Epoch [{}/{}], Loss: {:.4f}'.format(epoch + 1, num_epochs,
+                                                   loss.item()))
+# model = torch.load('model.ckpt')
+# Plot the graph
+predicted = model(torch.from_numpy(x_train)).detach().numpy()
+plt.plot(x_train, y_train, 'ro', label='Original data')
+plt.plot(x_train, predicted, label='Fitted line')
+plt.legend()
+plt.show()
+
+# Save the model checkpoint
+# torch.save(model, 'model.ckpt')
