c4cd2c57fc13f843eec46fbdf1da982d509ae08f
sp <995199235@qq.com>
2019-01-22 16:24:54 +0800

    transformer

--- My_Transformer/Transformer.py
+++ My_Transformer/Transformer.py
@@ -35,9 +35,81 @@ class Generator(nn.Module):
     """
     定义标准的线性+softmax生成step
     """
+
     def __init__(self, d_model, vocab):
         super(Generator, self).__init__()
         self.proj = nn.Linear(d_model, vocab)
 
     def forward(self, x):
         return F.log_softmax(self.proj(x), dim=-1)
+
+
+def clones(module, N):
+    """
+    生成n个相同的层
+    :param module:层
+    :param N:需要生成的个数
+    :return:nn.moduleList
+    """
+    return nn.ModuleList([copy.deepcopy(module)] for _ in range(N))
+
+
+class Encoder(nn.Module):
+    """
+    Core encoder is a stack of N layers
+    """
+
+    def __init__(self, layer, N):
+        super(Encoder, self).__init__()
+        self.layers = clones(layer, N)
+        self.norm = LayerNorm(layer.size)
+
+    def forward(self, x, mask):
+        # pass the input (and mask) through each layer in turn
+        for layer in self.layers:
+            x = layer(x, mask)
+        return self.norm(x)
+
+
+class LayerNorm(nn.Module):
+    """
+    Construct a layernorm module
+    """
+
+    def __init__(self, features, eps=1e-6):
+        super(LayerNorm, self).__init__()
+        self.a_2 = nn.Parameter(torch.ones(features))
+        self.b_2 = nn.Parameter(torch.zeros(features))
+        self.eps = eps
+
+    def forward(self, x):
+        mean = x.mean(-1, keepdim=True)
+        # std为标准差
+        std = x.std(-1, keepdim=True)
+        # 0均值标准化
+        return self.a_2 * (x - mean) / (std + self.eps) + self.b_2
+
+
+class SublayerConnection(nn.Module):
+    """
+    A residual connection followed by a layer norm.
+    Note for code simplicity the norm is first as opposed to last.!!!!!!!!!!!!!!!!!!!!!!!!!
+    """
+
+    def __init__(self, size, dropout):
+        super(SublayerConnection, self).__init__()
+        self.norm = LayerNorm(size)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x, sublayer):
+        # Apply residual connection to any sublayer with the same size.
+        return x + self.dropout(sublayer(self.norm(x)))
+
+
+class EncoderLayer(nn.Module):
+    """
+    Encoder is made up of self-attn and feed forward (defined below)
+    """
+
+if __name__ == '__main__':
+    print('test')
