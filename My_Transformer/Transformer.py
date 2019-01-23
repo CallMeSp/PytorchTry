41b1cda5b35c9399ea9dd2af906da82379af1874
sp <995199235@qq.com>
2019-01-23 22:35:10 +0800

    emmm

--- My_Transformer/Transformer.py
+++ My_Transformer/Transformer.py
@@ -211,7 +211,56 @@ class MultiHeadedAttention(nn.Module):
         return self.linears[-1](x)
 
 
+class PositionwiseFeedForward(nn.Module):
+    # Implements FFN equation.
+    def __init__(self, d_model, d_ff, dropout=0.1):
+        super(PositionwiseFeedForward, self).__init__()
+        self.w_1 = nn.Linear(d_model, d_ff)
+        self.w_2 = nn.Linear(d_ff, d_model)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x):
+        return self.w_2(self.dropout(F.relu(self.w_1(x))))
+
+
+class Embeddings(nn.Module):
+    def __init__(self, d_model, vocab):
+        super(Embeddings, self).__init__()
+        self.lut = nn.Embedding(vocab, d_model)
+        self.d_model = d_model
+
+    def forward(self, x):
+        return self.lut(x) * math.sqrt(self.d_model)
+
+
+class PositionalEncoding(nn.Module):
+    # Implement the PE function.
+
+    def __init__(self, d_model, dropout, max_len=5000):
+        super(PositionalEncoding, self).__init__()
+        self.dropout = nn.Dropout(p=dropout)
+
+        # Compute the positional encodings once in log space.
+        pe = torch.zeros(max_len, d_model)
+        position = torch.arange(0, max_len).unsqueeze(1).float()
+        div_term = torch.exp((torch.arange(0, d_model, 2) *
+                              -(math.log(10000.0) / d_model)).float())
+        pe[:, 0::2] = torch.sin(position * div_term)
+        pe[:, 1::2] = torch.cos(position * div_term)
+        pe = pe.unsqueeze(0)
+        self.register_buffer('pe', pe)
+
+    def forward(self, x):
+        x = x + Variable(self.pe[:, :x.size(1)],
+                         requires_grad=False)
+        return self.dropout(x)
+
+
 if __name__ == '__main__':
-    plt.figure(figsize=(5, 5))
-    plt.imshow(subsequent_mask(20)[0])
+    plt.figure(figsize=(15, 5))
+    pe = PositionalEncoding(20, 0)
+    y = pe.forward(Variable(torch.zeros(1, 100, 20)))
+    print(y.size())
+    plt.plot(np.arange(100), y[0, :, 4:8].data.numpy())
+    plt.legend(["dim %d" % p for p in [4, 5, 6, 7]])
     plt.show()
