50acfce55caad9061c4e89e8ac75e4e25a202ee0
sp <995199235@qq.com>
2019-04-24 12:46:23 +0800

    transformer

--- someTest/pre-trained-bert_Transformer.py
+++ someTest/pre-trained-bert_Transformer.py
@@ -1,3 +1,5 @@
+import os
+os.environ["CUDA_VISIBLE_DEVICES"] = "1"
 import torch
 from pytorch_pretrained_bert import TransfoXLTokenizer, TransfoXLModel, TransfoXLLMHeadModel
 
@@ -7,27 +9,31 @@ import logging
 logging.basicConfig(level=logging.INFO)
 
 # Load pre-trained model tokenizer (vocabulary from wikitext 103)
-tokenizer = TransfoXLTokenizer.from_pretrained('transfo-xl-wt103')
+tokenizer = TransfoXLTokenizer.from_pretrained('data')
 
 # Tokenized input
 text_1 = "Who was Jim Henson ?"
 text_2 = "Jim Henson was a puppeteer"
+text_3 = [text_1,text_2]
 tokenized_text_1 = tokenizer.tokenize(text_1)
 tokenized_text_2 = tokenizer.tokenize(text_2)
-
+tokenized_text_3 = [tokenizer.tokenize(sent) for sent in text_3]
 # Convert token to vocabulary indices
 indexed_tokens_1 = tokenizer.convert_tokens_to_ids(tokenized_text_1)
 indexed_tokens_2 = tokenizer.convert_tokens_to_ids(tokenized_text_2)
-
-print(indexed_tokens_1)
-print(indexed_tokens_2)
+indexed_tokens_3 = [tokenizer.convert_tokens_to_ids(sent) for sent in tokenized_text_3]
 
 # Convert inputs to PyTorch tensors
 tokens_tensor_1 = torch.tensor([indexed_tokens_1])
 tokens_tensor_2 = torch.tensor([indexed_tokens_2])
-
+tokens_tensor_3 = torch.tensor([indexed_tokens_3])
+print(tokens_tensor_1)
+print(tokens_tensor_2)
+print(tokens_tensor_3)
 # Load pre-trained model (weights)
-model = TransfoXLModel.from_pretrained('transfo-xl-wt103')
+
+
+model = TransfoXLModel.from_pretrained('data')
 model.eval()
 
 # If you have a GPU, put everything on cuda
@@ -42,19 +48,23 @@ with torch.no_grad():
     hidden_states_2, mems_2 = model(tokens_tensor_2, mems=mems_1)
 
 # Load pre-trained model (weights)
-model = TransfoXLLMHeadModel.from_pretrained('transfo-xl-wt103')
+model = TransfoXLLMHeadModel.from_pretrained('data')
 model.eval()
 
 # If you have a GPU, put everything on cuda
-tokens_tensor_1 = tokens_tensor_1.to('cuda')
-tokens_tensor_2 = tokens_tensor_2.to('cuda')
-model.to('cuda')
+if torch.cuda.is_available():
+    tokens_tensor_1 = tokens_tensor_1.to('cuda')
+    tokens_tensor_2 = tokens_tensor_2.to('cuda')
+    model.to('cuda')
+
 
 with torch.no_grad():
     # Predict all tokens
     predictions_1, mems_1 = model(tokens_tensor_1)
     # We can re-use the memory cells in a subsequent call to attend a longer context
     predictions_2, mems_2 = model(tokens_tensor_2, mems=mems_1)
+    print(predictions_1)
+    print(predictions_2)
 
 # get the predicted last token
 predicted_index = torch.argmax(predictions_2[0, -1, :]).item()
